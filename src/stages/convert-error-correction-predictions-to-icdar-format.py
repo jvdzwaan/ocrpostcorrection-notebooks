968c6fe4636b1c866ab51b2f43856f46f3965127
Janneke van der Zwaan <jannekevdzwaan@gmail.com>
2023-07-17 20:39:58 +0200

    Add CLI for converting correction predictions to the icdar format
    As this command requires a json file as input, I'm going to add a pipeline
    step for generating a json file with the perfect error detection results
    and update the pipeline step for generating the predictions.

---
+++ src/stages/convert-error-correction-predictions-to-icdar-format.py
@@ -0,0 +1,32 @@
+import json
+import sys
+from typing import Text
+
+import pandas as pd
+import typer
+from loguru import logger
+from typing_extensions import Annotated
+
+
+def convert_to_icdar_output(
+    predictions: Annotated[Text, typer.Option()],
+    icdar_json: Annotated[Text, typer.Option()],
+    out_file: Annotated[Text, typer.Option()],
+    loglevel: Annotated[str, typer.Option()],
+) -> None:
+    logger.remove()
+    logger.add(sys.stderr, level=loglevel)
+
+    with open(icdar_json) as f:
+        output = json.load(f)
+    data = pd.read_csv(predictions, index_col=0)
+
+    for _i, row in data.iterrows():
+        output[row.text][row.token][row.pred] = 1.0
+
+    with open(out_file, "w") as f:
+        json.dump(output, f)
+
+
+if __name__ == "__main__":
+    typer.run(convert_to_icdar_output)
