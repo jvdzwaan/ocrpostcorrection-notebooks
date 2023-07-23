9ec3628e12e1ffb0ee3f9f2f4164122dd425bfef
Janneke van der Zwaan <jannekevdzwaan@gmail.com>
2023-07-23 20:43:35 +0200

    Put typer option types in shared module so they can be reused

---
+++ src/common/option_types.py
@@ -0,0 +1,18 @@
+import typer
+
+file_in_option = typer.Option(
+    exists=True,
+    file_okay=True,
+    dir_okay=False,
+    writable=False,
+    readable=True,
+    resolve_path=True,
+)
+file_out_option = typer.Option(
+    exists=False,
+    file_okay=True,
+    dir_okay=False,
+    writable=True,
+    readable=True,
+    resolve_path=True,
+)
