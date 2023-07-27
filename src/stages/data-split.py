2a2136b3ea62b4fb5775302e11e332ba92fd97ad
Janneke van der Zwaan <jannekevdzwaan@gmail.com>
2023-07-27 21:53:32 +0200

    Update data-spilt to typer and options instead of config.yaml

--- src/stages/data-split.py
+++ src/stages/data-split.py
@@ -1,46 +1,44 @@
-import argparse
-import sys
 from pathlib import Path
-from typing import Text
 
-import yaml
+import typer
 from loguru import logger
 from ocrpostcorrection.icdar_data import get_intermediate_data
 from sklearn.model_selection import train_test_split
+from typing_extensions import Annotated
+
+from common.option_types import file_in_option, file_out_option
 
 
-def data_split(config_path: Text) -> None:
-    config = yaml.safe_load(open(config_path))
+def data_split(
+    raw_dataset: Annotated[Path, file_in_option],
+    train_split: Annotated[Path, file_out_option],
+    val_split: Annotated[Path, file_out_option],
+    test_split: Annotated[Path, file_out_option],
+    val_size: Annotated[float, typer.Option()],
+    seed: Annotated[int, typer.Option()],
+) -> None:
+    train_split.parent.mkdir(exist_ok=True, parents=True)
 
-    logger.remove()
-    logger.add(sys.stderr, level=config["base"]["loglevel"])
-
-    Path(config["data-split"]["train-split"]).parent.mkdir(exist_ok=True, parents=True)
-
-    _, md, _, md_test = get_intermediate_data(config["base"]["raw-data-zip"])
+    _, md, _, md_test = get_intermediate_data(raw_dataset)
 
     X_train, X_val, _, _ = train_test_split(
         md,
         md["file_name"],
-        test_size=config["data-split"]["val-size"],
-        random_state=config["base"]["seed"],
+        test_size=val_size,
+        random_state=seed,
         stratify=md["subset"],
     )
 
-    X_train.to_csv(config["data-split"]["train-split"])
-    X_val.to_csv(config["data-split"]["val-split"])
+    X_train.to_csv(train_split)
+    X_val.to_csv(val_split)
 
     logger.info(f"Train set contains {X_train.shape[0]} texts")
     logger.info(f"Val set contains {X_val.shape[0]} texts")
 
-    md_test.to_csv(config["data-split"]["test-split"])
+    md_test.to_csv(test_split)
 
     logger.info(f"Test set contains {md_test.shape[0]} texts")
 
 
 if __name__ == "__main__":
-    args_parser = argparse.ArgumentParser()
-    args_parser.add_argument("--config", dest="config", required=True)
-    args = args_parser.parse_args()
-
-    data_split(args.config)
+    typer.run(data_split)
