bae13d0704abbcb3bb270d98159ea4bfbefe68e1
Janneke van der Zwaan <jannekevdzwaan@gmail.com>
2023-07-28 14:26:32 +0200

    Update run-icdar-evaluation cli to use options
    And remove the loglevel parameter

--- src/stages/run-icdar-evaluation.py
+++ src/stages/run-icdar-evaluation.py
@@ -1,22 +1,23 @@
-import sys
 import tempfile
-from typing import Text
+from pathlib import Path
 
 import typer
 from loguru import logger
 from ocrpostcorrection.icdar_data import extract_icdar_data
 from ocrpostcorrection.utils import runEvaluation
+from typing_extensions import Annotated
+
+from common.option_types import file_in_option, file_out_option
 
 
 def run_icdar_evaluation(
-    raw_data_zip: Text, json_file: Text, csv_file: Text, loglevel: str
+    raw_dataset: Annotated[Path, file_in_option],
+    json_file: Annotated[Path, file_in_option],
+    csv_file: Annotated[Path, file_out_option],
 ) -> None:
-    logger.remove()
-    logger.add(sys.stderr, level=loglevel)
-
     logger.info("Running evaluation")
     with tempfile.TemporaryDirectory() as tmp_dir:
-        _, test_path = extract_icdar_data(tmp_dir, raw_data_zip)
+        _, test_path = extract_icdar_data(tmp_dir, raw_dataset)
 
         runEvaluation(test_path, json_file, csv_file)
 
