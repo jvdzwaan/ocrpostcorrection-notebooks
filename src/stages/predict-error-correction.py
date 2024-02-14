14a8d1aa94fcf469283e16abc0c11fb5393e0782
Janneke van der Zwaan <jannekevdzwaan@gmail.com>
2024-02-14 22:04:50 +0100

    Refactor predict error correction for (by)t5

--- src/stages/predict-error-correction.py
+++ src/stages/predict-error-correction.py
@@ -1,21 +1,14 @@
 import json
 import tempfile
-from typing import Any, Callable, Dict, Text, Tuple
+from pathlib import Path
+from typing import Dict, Text
 
 import edlib
 import pandas as pd
-import torch
 import typer
+from datasets import Dataset
 from loguru import logger
-from ocrpostcorrection.error_correction import (
-    SimpleCorrectionDataset,
-    SimpleCorrectionSeq2seq,
-    collate_fn,
-    generate_vocabs,
-    get_text_transform,
-    predict_and_convert_to_str,
-    validate_model,
-)
+from ocrpostcorrection.error_correction_t5 import filter_max_len, preprocess_function
 from ocrpostcorrection.icdar_data import Text as ICDARText
 from ocrpostcorrection.icdar_data import (
     extract_icdar_data,
@@ -23,62 +16,26 @@ from ocrpostcorrection.icdar_data import (
     normalized_ed,
 )
 from ocrpostcorrection.utils import icdar_output2simple_correction_dataset_df
-from torch.utils.data import DataLoader
-from torchtext.vocab import Vocab
+from transformers import (
+    AutoModelForSeq2SeqLM,
+    AutoTokenizer,
+    DataCollatorForSeq2Seq,
+    Seq2SeqTrainer,
+    Seq2SeqTrainingArguments,
+)
 from typing_extensions import Annotated
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-
-def load_model(
-    input_size: int,
-    hidden_size: int,
-    output_size: int,
-    dropout: float,
-    max_len: int,
-    teacher_forcing_ratio: float,
-    model_path: Text,
-) -> Tuple[SimpleCorrectionSeq2seq, torch.optim.Optimizer]:
-    model = SimpleCorrectionSeq2seq(
-        input_size=input_size,
-        hidden_size=hidden_size,
-        output_size=output_size,
-        dropout=dropout,
-        max_length=max_len,
-        teacher_forcing_ratio=teacher_forcing_ratio,
-        device=device,
-    )
-
-    optimizer = torch.optim.Adam(model.parameters())
-
-    checkpoint = torch.load(model_path, map_location=torch.device(device))
-    model.load_state_dict(checkpoint["model_state_dict"])
-    optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
-
-    return model, optimizer
-
-
-def create_test_dataloader(
-    test: pd.DataFrame,
-    max_len: int,
-    batch_size: int,
-    text_transform: Dict[str, Callable[[Any], Any]],
-) -> DataLoader:
-    test_dataset = SimpleCorrectionDataset(test, max_len=max_len)
-    test_dataloader = DataLoader(
-        test_dataset, batch_size=batch_size, collate_fn=collate_fn(text_transform)
-    )
-    logger.info(f"# test samples: {len(test_dataset)}")
-
-    return test_dataloader
+from common.option_types import file_in_option
 
 
 def create_predictions_csv(
-    test: pd.DataFrame, max_len: int, predictions: pd.DataFrame
+    test: pd.DataFrame, max_len: int, predictions: pd.DataFrame, dev: bool = False
 ) -> pd.DataFrame:
     test_results = (
         test.query(f"len_ocr <= {max_len}").query(f"len_gs <= {max_len}").copy()
     )
+    if dev:
+        test_results = test_results[:5]
     test_results["pred"] = predictions
 
     test_results["ed"] = test_results.apply(
@@ -102,26 +59,28 @@ def predict_and_save(
     in_file: Text,
     data_test: Dict[str, ICDARText],
     max_len: int,
-    batch_size: int,
-    text_transform: Dict[str, Callable[[Any], Any]],
-    vocab_transform: Dict[str, Vocab],
-    model: SimpleCorrectionSeq2seq,
+    trainer,
+    tokenizer,
     out_file: Text,
+    dev: bool = False,
 ) -> None:
     logger.info(f"Generating predictions for '{in_file}'")
     if in_file and out_file:
         with open(in_file) as f:
             output = json.load(f)
         test = icdar_output2simple_correction_dataset_df(output, data_test)
-        test_dataloader = create_test_dataloader(
-            test, max_len, batch_size, text_transform
+
+        dataset = Dataset.from_pandas(test)
+        if dev:
+            dataset = dataset.select(range(5))
+        tokenized_dataset = dataset.map(
+            preprocess_function, fn_kwargs={"tokenizer": tokenizer}, batched=True
         )
 
-        predictions = predict_and_convert_to_str(
-            model, test_dataloader, vocab_transform["gs"], device
-        )
+        pred = trainer.predict(tokenized_dataset)
+        predictions = tokenizer.batch_decode(pred.predictions, skip_special_tokens=True)
 
-        results = create_predictions_csv(test, max_len, predictions)
+        results = create_predictions_csv(test, max_len, predictions, dev)
         results.to_csv(out_file)
         logger.info(f"Saved predictions to '{out_file}'")
     elif in_file or out_file:
@@ -132,56 +91,19 @@ def predict_and_save(
 
 
 def predict_error_correction(
-    error_correction_dataset: Annotated[Text, typer.Option()],
-    hidden_size: Annotated[int, typer.Option()],
-    dropout: Annotated[float, typer.Option()],
+    error_correction_dataset: Annotated[Path, file_in_option],
     max_len: Annotated[int, typer.Option()],
-    teacher_forcing_ratio: Annotated[float, typer.Option()],
-    model_path: Annotated[Text, typer.Option()],
-    batch_size: Annotated[int, typer.Option()],
+    model_name: Annotated[Text, typer.Option()],
     calculate_loss: Annotated[bool, typer.Option()],
-    test_log_file: Annotated[Text, typer.Option()],
-    raw_dataset: Annotated[Text, typer.Option()],
+    test_log_file: Annotated[Path, typer.Option()],
+    raw_dataset: Annotated[Path, file_in_option],
+    seed: Annotated[int, typer.Option()],
     perfect_detection_in: Annotated[Text, typer.Option()] = "",
     perfect_detection_out: Annotated[Text, typer.Option()] = "",
     predicted_detection_in: Annotated[Text, typer.Option()] = "",
     predicted_detection_out: Annotated[Text, typer.Option()] = "",
+    dev: Annotated[bool, typer.Option()] = False,
 ) -> None:
-    # Load data
-    logger.info("Loading data")
-    data = pd.read_csv(error_correction_dataset, index_col=0)
-    data = data.fillna("")
-
-    train = data.query('dataset == "train"')
-    test = data.query('dataset == "test"')
-
-    logger.info(f"Train: {train.shape[0]} samples; test: {test.shape[0]} samples")
-
-    # Load model
-    logger.info("Loading model")
-    vocab_transform = generate_vocabs(train)
-    text_transform = get_text_transform(vocab_transform)
-
-    model, _optimizer = load_model(
-        input_size=len(vocab_transform["ocr"]),
-        hidden_size=hidden_size,
-        output_size=len(vocab_transform["gs"]),
-        dropout=dropout,
-        max_len=max_len,
-        teacher_forcing_ratio=teacher_forcing_ratio,
-        model_path=model_path,
-    )
-    model.to(device)
-
-    if calculate_loss:
-        logger.info("Calculating loss on dataset without duplicates")
-        test_dataloader = create_test_dataloader(
-            test, max_len, batch_size, text_transform
-        )
-        test_loss = validate_model(model, test_dataloader, device)
-        test_log = pd.DataFrame({"test_loss": [test_loss]})
-        test_log.to_csv(test_log_file)
-
     logger.info("Loading the test dataset")
     with tempfile.TemporaryDirectory() as tmp_dir:
         _train_path, test_path = extract_icdar_data(tmp_dir, raw_dataset)
@@ -190,26 +112,65 @@ def predict_error_correction(
         data_test, _md_test = generate_data(test_path)
         logger.info(f"Found {len(data_test)} texts in test data")
 
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    data_collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, model=model_name)
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+
+    training_args = Seq2SeqTrainingArguments(
+        seed=seed,
+        output_dir=model_name,
+        predict_with_generate=True,
+    )
+
+    trainer = Seq2SeqTrainer(
+        model=model,
+        args=training_args,
+        tokenizer=tokenizer,
+        data_collator=data_collator,
+    )
+
+    if calculate_loss:
+        logger.info("Calculating loss on dataset without duplicates")
+        data = pd.read_csv(error_correction_dataset, index_col=0)
+        data.fillna("", inplace=True)
+
+        dataset = Dataset.from_pandas(data.query('dataset == "test"'))
+
+        dataset = dataset.filter(
+            filter_max_len, fn_kwargs={"max_len": max_len}, batched=False
+        )
+
+        if dev:
+            dataset = dataset.select(range(5))
+
+        tokenized_dataset = dataset.map(
+            preprocess_function, fn_kwargs={"tokenizer": tokenizer}, batched=True
+        )
+
+        pred = trainer.predict(tokenized_dataset)
+
+        logs = pd.DataFrame([pred.metrics])
+        (test_log_file.parent).mkdir(exist_ok=True, parents=True)
+        logs.to_csv(test_log_file)
+
     predict_and_save(
         perfect_detection_in,
         data_test,
         max_len,
-        batch_size,
-        text_transform,
-        vocab_transform,
-        model,
+        trainer,
+        tokenizer,
         perfect_detection_out,
+        dev,
     )
 
     predict_and_save(
         predicted_detection_in,
         data_test,
         max_len,
-        batch_size,
-        text_transform,
-        vocab_transform,
-        model,
+        trainer,
+        tokenizer,
         predicted_detection_out,
+        dev,
     )
 
 
