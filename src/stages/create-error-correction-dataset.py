067615ea05683195695016d5a3d542e6e80c829d
Janneke van der Zwaan <jannekevdzwaan@gmail.com>
2024-03-22 16:16:11 +0100

    Update create error correction dataset to add context for a mistake

--- src/stages/create-error-correction-dataset.py
+++ src/stages/create-error-correction-dataset.py
@@ -4,7 +4,10 @@ from typing import Optional
 import pandas as pd
 import typer
 from loguru import logger
-from ocrpostcorrection.error_correction import get_tokens_with_OCR_mistakes
+from ocrpostcorrection.error_correction import (
+    get_OCR_mistakes_in_context,
+    get_tokens_with_OCR_mistakes,
+)
 from ocrpostcorrection.icdar_data import get_intermediate_data
 from typing_extensions import Annotated
 
@@ -15,6 +18,7 @@ def create_error_correction_dataset(
     raw_dataset: Annotated[Path, file_in_option],
     val_split: Annotated[Path, file_in_option],
     dataset_out: Annotated[Path, file_out_option],
+    context_offset: Annotated[int, typer.Option()],
     include_language: Annotated[
         Optional[bool], typer.Option("--include-language/--exclude-language")
     ] = False,
@@ -26,10 +30,23 @@ def create_error_correction_dataset(
 
     logger.info("Getting tokens with OCR mistakes")
     tdata = get_tokens_with_OCR_mistakes(data, data_test, list(X_val.file_name))
+    logger.info(f"Found {tdata.shape[0]} samples.")
+    if context_offset > 0:
+        logger.info(f"Adding context of ~{context_offset} characters")
+        cdata = get_OCR_mistakes_in_context(
+            data,
+            data_test,
+            tdata,
+            context_offset,
+        )
+        tdata = tdata.merge(cdata)
 
     subset = ["ocr", "gs", "dataset"]
     if include_language:
         subset.append("language")
+    if context_offset > 0:
+        subset.append("context_before")
+        subset.append("context_after")
     logger.info(f"Subset for identyfying duplicate samples: {subset}")
 
     tdata.drop_duplicates(subset=subset, inplace=True)
